cd655348e2e895be77dc4f7b05244ae2b43fbd7c
runs-15 <harunsixsixfour@gmail.com>
2022-02-25 17:50:08 +0700

    v.2.2.3 Fixing levelling modules

--- bot/modules/exp/cog.py
+++ bot/modules/exp/cog.py
@@ -448,7 +448,7 @@ class Exp(Cog):
                 embed.add_field(name="Rank", value=f"**{int(ranking)}**{'st' if str(int(ranking))[-1] == '1' else ('nd' if str(int(ranking))[-1] == '2' else ('rd' if str(int(ranking))[-1] == '3' else 'th'))} of {len([m for m in user.guild.members if not m.bot])}", inline=True)
                 #embed.add_field(name="Progress Bar", value=boxes * color + (20-boxes) * ":white_large_square:", inline=False)
                 embed.set_image(url=f"attachment://{user.id}.jpg")
-                embed.set_thumbnail(url=user.avatar_url)
+                embed.set_thumbnail(url=user.avatar.url)
                 await ctx.send(file=file, embed=embed)
             else:
                 await ctx.send("User ini belum bergabung dalam voice chat!")
@@ -504,7 +504,7 @@ class Exp(Cog):
                 embed.add_field(name="Rank", value=f"**{int(ranking)}**{'st' if str(int(ranking))[-1] == '1' else ('nd' if str(int(ranking))[-1] == '2' else ('rd' if str(int(ranking))[-1] == '3' else 'th'))} of {len([m for m in ctx.guild.members if not m.bot])}", inline=True)
                 #embed.add_field(name="Progress Bar", value=boxes * color + (20-boxes) * ":white_large_square:", inline=False)
                 embed.set_image(url=f"attachment://{ctx.author.id}.jpg")
-                embed.set_thumbnail(url=ctx.author.avatar_url)
+                embed.set_thumbnail(url=ctx.author.avatar.url)
                 await ctx.send(file=file, embed=embed)
             else:
                 await ctx.send("Bergabunglah dalam channel voice chat terlebih dahulu!")
