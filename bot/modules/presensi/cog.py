dd0c2509fe22c4ee1f94c42a4e014ab665d62478
runs-15 <harunsixsixfour@gmail.com>
2022-03-09 09:50:47 +0700

    .

--- bot/modules/presensi/cog.py
+++ bot/modules/presensi/cog.py
@@ -334,7 +334,7 @@ class Presensi(Cog):
         
         kirim2 = ''
         for i in ['GAMMA NASIM', 'MUHAMMAD RAFIF HANIFA', 'MUHAMMAD RODHIYAN RIJALUL WAHID', 'RAMA ANDHIKA PRATAMA', 'HARUN', 'MUSA GANI RAHMAN', 'EVANDHIKA AGNA MAULANA', 'IRFAN SURYA RAMADHAN', 'MUHAMMAD DZAKY ASRAF', 'RAYHAN ERSA NOVARDHANA', 'HIKMAT SEJATI', 'TAZAKKA ARIFIN NUTRIATMA', 'LANANG BASWARA SAKHI', 'DZAKI SENTANU NURAGUSTA', 'RIZQI ILHAM MAULANA', 'ALVINENDRA TRIAJI WIBOWO']:
-            kirim2 += "{:<32}: {:<5} / 100 {:<3}\n".format(i, xi.query(f"Nama == '{i}'")['Nilai'].values[0], xi.query(f"Nama == '{i}'")['Rank'].values[0])
+            kirim2 += "{:<32}: {:<5} / 100    {:<3}\n".format(i, xi.query(f"Nama == '{i}'")['Nilai'].values[0], xi.query(f"Nama == '{i}'")['Rank'].values[0])
         
         kirim3 = xi['Nilai'].describe()
         
@@ -364,7 +364,7 @@ class Presensi(Cog):
                         kirim2 = ''
                         for i in ['GAMMA NASIM', 'MUHAMMAD RAFIF HANIFA', 'MUHAMMAD RODHIYAN RIJALUL WAHID', 'RAMA ANDHIKA PRATAMA', 'HARUN', 'MUSA GANI RAHMAN', 'EVANDHIKA AGNA MAULANA', 'IRFAN SURYA RAMADHAN', 'MUHAMMAD DZAKY ASRAF', 'RAYHAN ERSA NOVARDHANA', 'HIKMAT SEJATI', 'TAZAKKA ARIFIN NUTRIATMA', 'LANANG BASWARA SAKHI', 'DZAKI SENTANU NURAGUSTA', 'RIZQI ILHAM MAULANA', 'ALVINENDRA TRIAJI WIBOWO']:
                             try:
-                                kirim2 += "{:<32}: {:<4} / 100 {:<3}\n".format(i, xi.query(f"Nama == '{i}'")['Nilai'].values[0], xi.query(f"Nama == '{i}'")['Rank'].values[0])
+                                kirim2 += "{:<32}: {:<4} / 100    {:<3}\n".format(i, xi.query(f"Nama == '{i}'")['Nilai'].values[0], xi.query(f"Nama == '{i}'")['Rank'].values[0])
                             except:
                                 pass
                             
