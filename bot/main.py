b130fcba2e6096539cf95c22306d84cf32721a18
runs-15 <harunsixsixfour@gmail.com>
2022-03-12 17:59:28 +0700

    v.2.3.8 Changed bot system

--- bot/main.py
+++ bot/main.py
@@ -25,38 +25,38 @@ for folder in os.listdir("bot/modules"):
         bot.load_extension(f"modules.{folder}.cog")
         print(f"    {folder} cogs loaded!")
         
-def syntax(command):
-    cmd_and_aliases = "|".join([str(command), *command.aliases])
-    params = []
+# def syntax(command):
+#     cmd_and_aliases = "|".join([str(command), *command.aliases])
+#     params = []
     
-    for key, value in command.params.items():
-        if key not in ("self", "ctx"):
-            params.append(f"[{key}]" if "=" in str(value) else f"<{key}>")
-    params = " ".join(params)
-    return f"```{cmd_and_aliases} {params}```"
+#     for key, value in command.params.items():
+#         if key not in ("self", "ctx"):
+#             params.append(f"[{key}]" if "=" in str(value) else f"<{key}>")
+#     params = " ".join(params)
+#     return f"```{cmd_and_aliases} {params}```"
         
-async def cmd_help(ctx, command):
-    embed = Embed(title=f"Bantuan perintah `{command}`", description=syntax(command), colour=int(hex(int("2f3136", 16)), 0), timestamp=datetime.datetime.now())
-    embed.add_field(name="Deskripsi perintah: ", value=command.help)
-    embed.set_author(name=bot.user.name, icon_url=bot.user.avatar.url)
-    await ctx.send(embed=embed)
+# async def cmd_help(ctx, command):
+#     embed = Embed(title=f"Bantuan perintah `{command}`", description=syntax(command), colour=int(hex(int("2f3136", 16)), 0), timestamp=datetime.datetime.now())
+#     embed.add_field(name="Deskripsi perintah: ", value=command.help)
+#     embed.set_author(name=bot.user.name, icon_url=bot.user.avatar.url)
+#     await ctx.send(embed=embed)
     
-async def on_command_error(ctx, exc):
-    if isinstance(exc, discord.ext.commands.CommandNotFound):
-        cmd_lst = ''
-        for i in bot.commands:
-            if ctx.message.content.split()[0].lstrip(db.servers_con['servers']['server'].find({'server_id' : ctx.guild.id})[0]['prefix']) in i.name or ctx.message.content.split()[0].lstrip(db.servers_con['servers']['server'].find({'server_id' : ctx.guild.id})[0]['prefix']) in ' '.join(i.aliases):
-                cmd_lst += f'`{i or " ".join(i.aliases)}` '
-                continue
-        if cmd_lst != '':
-            hlp = await ctx.reply(f"Command not available, or maybe call {cmd_lst}instead?")
-        else:
-            hlp = await ctx.reply("Command not available")
-    else:
-        print(exc)
+# async def on_command_error(ctx, exc):
+#     if isinstance(exc, discord.ext.commands.CommandNotFound):
+#         cmd_lst = ''
+#         for i in bot.commands:
+#             if ctx.message.content.split()[0].lstrip(db.servers_con['servers']['server'].find({'server_id' : ctx.guild.id})[0]['prefix']) in i.name or ctx.message.content.split()[0].lstrip(db.servers_con['servers']['server'].find({'server_id' : ctx.guild.id})[0]['prefix']) in ' '.join(i.aliases):
+#                 cmd_lst += f'`{i or " ".join(i.aliases)}` '
+#                 continue
+#         if cmd_lst != '':
+#             hlp = await ctx.reply(f"Command not available, or maybe call {cmd_lst}instead?")
+#         else:
+#             hlp = await ctx.reply("Command not available")
+#     else:
+#         print(exc)
                     
-        if (command := get(bot.commands, name=ctx.message.content.split()[0].lstrip(db.servers_con['servers']['server'].find({'server_id' : ctx.guild.id})[0]['prefix']))):
-            await ctx.reply(f"```{str(exc)}```", embed=await cmd_help(ctx, command))
+#         if (command := get(bot.commands, name=ctx.message.content.split()[0].lstrip(db.servers_con['servers']['server'].find({'server_id' : ctx.guild.id})[0]['prefix']))):
+#             await ctx.reply(f"```{str(exc)}```", embed=await cmd_help(ctx, command))
 
 @bot.event
 async def on_ready():
--- bot/modules/tools/cog.py
+++ bot/modules/tools/cog.py
@@ -230,39 +230,39 @@ class Tools(Cog):
         data = [x for x in cat]
         await ctx.send(f'**{random.choice(data)}**')
     
-    # def syntax(self, command):
-    #     cmd_and_aliases = "|".join([str(command), *command.aliases])
-    #     params = []
+    def syntax(self, command):
+        cmd_and_aliases = "|".join([str(command), *command.aliases])
+        params = []
         
-    #     for key, value in command.params.items():
-    #         if key not in ("self", "ctx"):
-    #             params.append(f"[{key}]" if "=" in str(value) else f"<{key}>")
-    #     params = " ".join(params)
-    #     return f"```{cmd_and_aliases} {params}```"
+        for key, value in command.params.items():
+            if key not in ("self", "ctx"):
+                params.append(f"[{key}]" if "=" in str(value) else f"<{key}>")
+        params = " ".join(params)
+        return f"```{cmd_and_aliases} {params}```"
             
-    # async def cmd_help(self, ctx, command):
-    #     embed = discord.Embed(title=f"Bantuan perintah `{command}`", description=self.syntax(command), colour=int(hex(int("2f3136", 16)), 0), timestamp=datetime.datetime.now())
-    #     embed.add_field(name="Deskripsi perintah: ", value=command.help)
-    #     embed.set_author(name=self.bot.user.name, icon_url=self.bot.user.avatar.url)
-    #     await ctx.send(embed=embed)
+    async def cmd_help(self, ctx, command):
+        embed = discord.Embed(title=f"Bantuan perintah `{command}`", description=self.syntax(command), colour=int(hex(int("2f3136", 16)), 0), timestamp=datetime.datetime.now())
+        embed.add_field(name="Deskripsi perintah: ", value=command.help)
+        embed.set_author(name=self.bot.user.name, icon_url=self.bot.user.avatar.url)
+        await ctx.send(embed=embed)
             
-    # @Cog.listener()
-    # async def on_command_error(self, ctx, exc):
-    #     if isinstance(exc, discord.ext.commands.CommandNotFound):
-    #         cmd_lst = ''
-    #         for i in self.bot.commands:
-    #             if ctx.message.content.split()[0].lstrip(db.servers_con['servers']['server'].find({'server_id' : ctx.guild.id})[0]['prefix']) in i.name or ctx.message.content.split()[0].lstrip(db.servers_con['servers']['server'].find({'server_id' : ctx.guild.id})[0]['prefix']) in ' '.join(i.aliases):
-    #                 cmd_lst += f'`{i or " ".join(i.aliases)}` '
-    #                 continue
-    #         if cmd_lst != '':
-    #             hlp = await ctx.reply(f"Command not available, or maybe call {cmd_lst}instead?")
-    #         else:
-    #             hlp = await ctx.reply("Command not available")
-    #     else:
-    #         print(exc)
+    @Cog.listener()
+    async def on_command_error(self, ctx, exc):
+        if isinstance(exc, discord.ext.commands.CommandNotFound):
+            cmd_lst = ''
+            for i in self.bot.commands:
+                if ctx.message.content.split()[0].lstrip(db.servers_con['servers']['server'].find({'server_id' : ctx.guild.id})[0]['prefix']) in i.name or ctx.message.content.split()[0].lstrip(db.servers_con['servers']['server'].find({'server_id' : ctx.guild.id})[0]['prefix']) in ' '.join(i.aliases):
+                    cmd_lst += f'`{i or " ".join(i.aliases)}` '
+                    continue
+            if cmd_lst != '':
+                hlp = await ctx.reply(f"Command not available, or maybe call {cmd_lst}instead?")
+            else:
+                hlp = await ctx.reply("Command not available")
+        else:
+            print(exc)
                         
-    #         if (command := get(self.bot.commands, name=ctx.message.content.split()[0].lstrip(db.servers_con['servers']['server'].find({'server_id' : ctx.guild.id})[0]['prefix']))):
-    #             await ctx.reply(f"```{str(exc)}```", embed=await self.cmd_help(ctx, command))
+            if (command := get(self.bot.commands, name=ctx.message.content.split()[0].lstrip(db.servers_con['servers']['server'].find({'server_id' : ctx.guild.id})[0]['prefix']))):
+                await ctx.reply(f"```{str(exc)}```", embed=await self.cmd_help(ctx, command))
     
 def setup(bot):
     bot.add_cog(Tools(bot))
