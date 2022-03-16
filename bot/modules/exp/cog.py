b949cf089f155f8885c303240cf125c5d2d0e3af
runs-15 <harunsixsixfour@gmail.com>
2022-03-16 16:29:52 +0700

    .

--- bot/modules/exp/cog.py
+++ bot/modules/exp/cog.py
@@ -174,8 +174,6 @@ class Exp(Cog):
                     # db.servers_con['servers']['social_credit'].update_one({'discord_id' : member.id}, {"$set": {'u_exp': pindahexp}})
                     # levelexp = db.servers_con['servers']['social_credit'].find({'discord_id' : member.id})[0]['v_level']
                     # db.servers_con['servers']['social_credit'].update_one({'discord_id' : member.id}, {"$set": {'u_level': levelexp}})
-                    
-                    db.servers_con['servers']['social_credit'].update_one({},{'$unset':{'v_time' : ''}})
                     db.servers_con['servers']['others'].update_many( {}, { '$rename': { "v_pity": "u_pity" } } )
                     db.servers_con['servers']['others'].update_many( {}, { '$rename': { "v_chance": "u_chance" } } )
                     
