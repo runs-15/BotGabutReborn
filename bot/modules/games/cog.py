7c61e8e98d57413d95eaaebf54c8316bd1825ded
runs-15 <harunsixsixfour@gmail.com>
2022-03-18 16:52:50 +0700

    .

--- bot/modules/games/cog.py
+++ bot/modules/games/cog.py
@@ -14,7 +14,7 @@ class Games(Cog):
     @cooldown(1, 60, BucketType.user)
     async def basic_math(self, ctx):
         """
-        > Shows a simple mathematic sentence. Your task is to answer correctly. Rewards `30 + (operator * 30) + (length each operand * 10)` exp if win and minus half of the rewards if lost. You should answer within `5 + (operator * 3) + (length each operand * 2)` seconds.
+        > Shows a simple mathematic sentence. Your task is to answer correctly. Rewards `30 + (operator * 10) + (length each operand * 10)` exp if win and minus half of the rewards if lost. You should answer within `5 + (operator * 3) + (length each operand * 2)` seconds.
 
         **Params:**
         >    takes no parameter
@@ -175,65 +175,6 @@ class Games(Cog):
             return await soal.edit(embed=Embed(title='Time Up!', description=f'Your exp was decreased by **`{-1/2 * exp_multiplier}`**'), delete_after=60)
 
         if int(guess.content) == int(answer) or float(guess.content) == float(answer):
-            db.add_exp(ctx.author.id, exp_multiplier)
-            
-            await soal.edit(embed=Embed(title='You got that!', description=f'Your exp was increased by **`{exp_multiplier}`**'), delete_after=60)
-            await guess.delete()
-            
-        else:
-            db.add_exp(ctx.author.id, -1/2*exp_multiplier)
-            await soal.edit(embed=Embed(title='Oops!', description=f'Your exp was decreased by **`{-1/2 * exp_multiplier}`**'), delete_after=60)
-            await guess.delete()
-            
-        await ctx.message.delete()
-    
-    @command(name='quiz.basic-math')
-    @cooldown(1, 60, BucketType.user)
-    async def basic_math(self, ctx):
-        """
-        > Shows a simple mathematic sentence. Your task is to answer correctly. Rewards `30 + (operator * 10) + (length each operand * 5)` exp if win and minus half of the rewards if lost. You should answer within `5 + (operator * 3) + (length each operand * 2)` seconds.
-
-        **Params:**
-        >    takes no parameter
-
-        **Returns:**
-        >    **`embed`** → math question
-
-        **Example:**
-        > ```<prefix>quiz.basic-math```
-        """
-        randomizer = random.randint(1, 6)
-        math_sentence = ''
-        operand = np.random.choice([x for x in range(1, 10)], randomizer + 1)
-        operator = np.random.choice(['+', '-', '*'], randomizer, p=[0.4, 0.4, 0.2])
-        
-        for i in range(randomizer):
-            math_sentence += f'{operand[i]} {operator[i]} '
-            
-        math_sentence += str(operand[randomizer])
-            
-        timeout = 5 + (len(operator) * 3) + (sum(len(str(x)) for x in operand) * 2)
-        exp_multiplier = 30 + (len(operator) * 10) + (sum(len(str(x)) for x in operand) * 5)
-        
-        answer = eval(math_sentence)
-
-        soal_embed = Embed(title = 'Answer this question!')
-        soal_embed.add_field(name='Math sentence', value=f'```{math_sentence}```', inline=True)
-        soal_embed.add_field(name='Answer Timeout', value=f'```{timeout} seconds```', inline=True)
-        soal_embed.add_field(name='Potential Reward', value=f'```{exp_multiplier} exp```', inline=True)
-        soal_embed.add_field(name='Clue', value=f'```The right answer has {len(str(answer))} digits```', inline=True)
-        soal = await ctx.reply(embed=soal_embed)
-
-        def is_correct(m):
-            return m.author == ctx.author and m.content.isdigit()
-
-        try:
-            guess = await self.bot.wait_for("message", check=is_correct, timeout=timeout)
-        except asyncio.TimeoutError:
-            db.add_exp(ctx.author.id, -1/2*exp_multiplier)
-            return await soal.edit(embed=Embed(title='Time Up!', description=f'Your exp was decreased by **`{-1/2 * exp_multiplier}`**'), delete_after=60)
-
-        if guess.content == answer:
             db.add_exp(ctx.author.id, exp_multiplier)
             
             await soal.edit(embed=Embed(title='You got that!', description=f'Your exp was increased by **`{exp_multiplier}`**'), delete_after=60)
